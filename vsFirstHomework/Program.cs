d44e21a41bc4f794baddcd1ffaf9976cac3d5238
kzoly <78027613+kzoly@users.noreply.github.com>
2021-08-28 14:28:15 +0300

    second commit

--- vsFirstHomework/Program.cs
+++ vsFirstHomework/Program.cs
@@ -12,18 +12,22 @@ namespace vsFirstHomework
             IShapeStore store = new ShapeStore();
 
 
-            Shape firstShape = new Shape("Rectangle");           
+            Shape firstShape = new Shape("Rectangle",3,5);           
             store.AddShape(firstShape);
+           //Console.WriteLine( firstShape.GetArea());
 
-            Shape secandShape = new Shape("Square");
+            Shape secandShape = new Shape("Square",4,4);
             store.AddShape(secandShape);
-            Shape thirdShape = new Shape("Circle");
+             Shape thirdShape = new Shape("Circle",7);
             store.AddShape(thirdShape);
+
+            Shape fourthShape = new Shape("Rectangle", 9, 11);
+            store.AddShape(fourthShape);
 
             var allShapes = store.GetAllShapes();
             foreach (var shape in allShapes)
             {
-                Console.WriteLine( shape.GetName());
+                Console.WriteLine( shape.GetName()+ " with area:" + shape.GetArea()+ " and with perimeter:" + shape.GetPerimeter());
 
             }
         }
--- vsFirstHomework/classes/Shape.cs
+++ vsFirstHomework/classes/Shape.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +10,48 @@ namespace vsFirstHomework.classes
   
     public class Shape
     {
+        //[JsonProperty("Name")]
         protected string Name { get; set; }
-        public Shape(string name)
+
+      //  [JsonProperty("A")]
+        protected int A { get; set; }
+      //  [JsonProperty("B")]
+        protected int B { get; set; }
+        public Shape(string name, int a, int b=1)
         {
             Name = name;
+            A = a;
+            B = b;
+            
         }
 
         public string GetName()
         {
-            return Name;
+           // return Name;
+            return JsonConvert.SerializeObject(this.Name);
+        }
+
+        public string GetArea()
+        {
+            double PI = 3.14;
+            if (this.Name == "Square" || this.Name == "Rectangle")
+                return (this.A + this.B).ToString();
+            else
+                if (this.Name == "Circle")
+                return  (PI * this.A*this.A).ToString();
+            else
+                return "Err of calculating area";
+        }
+        public string GetPerimeter()
+        {
+            double PI = 3.14;
+            if (this.Name == "Square" || this.Name == "Rectangle")
+                return (2*(this.A + this.B)).ToString();
+            else
+                if (this.Name == "Circle")
+                return (2 * PI * this.A).ToString();
+            else
+                return "Err of calculating perimeter";
         }
     }
 }
