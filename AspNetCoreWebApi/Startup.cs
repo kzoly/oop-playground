29ff7d23018973395cef8e75560d8742ebf67073
kzoly <78027613+kzoly@users.noreply.github.com>
2021-08-31 14:05:19 +0300

    Call an ASP.NET Core web API with JavaScript

--- AspNetCoreWebApi/Startup.cs
+++ AspNetCoreWebApi/Startup.cs
@@ -41,6 +41,9 @@ namespace AspNetCoreWebApi
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetCoreWebApi v1"));
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
